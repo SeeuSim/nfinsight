91fbe947b17c7c2520efee48b31b4f9eb0e58279
SeeuSim <seeusimong@gmail.com>
2023-05-13 19:24:44 +0800

    Add CosmosDB Cassandra

--- src/infrastructure/main.tf
+++ src/infrastructure/main.tf
@@ -11,11 +11,43 @@ terraform {
 
 provider "azurerm" {
   features {
-    
+
   }
 }
 
 resource "azurerm_resource_group" "rm" {
   name     = "NFInsight"
   location = "southeastasia"
+}
+
+resource "azurerm_cosmosdb_account" "db" {
+  name                = "nf-db"
+  location            = azurerm_resource_group.rm.location
+  resource_group_name = azurerm_resource_group.rm.name
+  offer_type          = "Standard"
+  enable_free_tier    = true
+
+  capabilities {
+    name = "EnableCassandra"
+  }
+
+  capacity {
+    total_throughput_limit = 300
+  }
+
+  backup {
+    type                = "Periodic"
+    interval_in_minutes = 240
+    retention_in_hours  = 8
+    storage_redundancy  = "Geo"
+  }
+
+  consistency_policy {
+    consistency_level = "Strong"
+  }
+
+  geo_location {
+    location          = azurerm_resource_group.rm.location
+    failover_priority = 0
+  }
 }
