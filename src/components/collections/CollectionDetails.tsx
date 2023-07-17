4744ef8a48e03f999fa935110d18961037da94f0
SeeuSim <seeusimong@gmail.com>
2023-07-17 18:31:11 +0800

    Add base collection details page

--- src/components/collections/CollectionDetails.tsx
+++ src/components/collections/CollectionDetails.tsx
@@ -1,5 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
-import { QueryResult } from "pg";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+
+import CollectionArtwork from "@/components/images/CollectionArtwork";
+import { AspectRatio } from "@/components/ui/aspect-ratio";
 import { ICollectionMetaResult } from "@/lib/database/postgres/getCollectionMetadata";
 
 interface ICollectionDetailsProps {
@@ -42,10 +46,36 @@ const CollectionDetails = ({ collectionAddress }: ICollectionDetailsProps) => {
     </>
   ) : (
     <>
-      <div>
-        <span>{data.name}</span>
-        <img src={data.image} />
-        <img src={data.banner_image} />
+      <div className="">
+        <div className="relative mb-8 sm:mb-12">
+          <div className="max-h-[320px] w-full overflow-clip">
+            <AspectRatio ratio={2}>
+              <CollectionArtwork
+                src={data.banner_image}
+                alt="Collection Banner"
+                altSrc="/images/collection_banner_fallback.png"
+              />
+            </AspectRatio>
+          </div>
+          <div className="absolute left-4 top-full z-50 w-[128px] -translate-y-[80%] overflow-clip rounded-2xl border-4 border-gray-400 bg-gradient-to-tr from-fuchsia-200 to-blue-200 shadow-md sm:left-8 sm:w-[200px]">
+            <AspectRatio ratio={1}>
+              <CollectionArtwork src={data.image} alt="Collection Artwork" />
+            </AspectRatio>
+          </div>
+        </div>
+        <div className="px-4 sm:px-8">
+          <div className="flex w-full flex-col space-y-1 border-0 !border-transparent px-1 outline-none sm:space-y-2">
+            <span className="text-xl font-bold sm:text-4xl">{data.name}</span>
+            <div>
+              <ReactMarkdown
+                className="prose prose-rose text-sm prose-h1:font-black prose-a:text-blue-600 sm:text-base sm:prose-a:font-light"
+                remarkPlugins={[remarkGfm]}
+              >
+                {data.description}
+              </ReactMarkdown>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
---
+++ src/components/images/CollectionArtwork.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+
+const CollectionArtwork = ({
+  src,
+  alt,
+  altSrc,
+}: React.HTMLProps<HTMLImageElement> & { altSrc?: string }) => {
+  const [isError, setIsError] = useState(false);
+  return (
+    <Image
+      className="flex-shrink-0 object-cover"
+      src={isError ? altSrc ?? "/images/collection_fallback.webp" : src ?? ""}
+      alt={alt ?? ""}
+      fill
+      onError={() => setIsError(true)}
+      quality={100}
+    />
+  );
+};
+
+export default CollectionArtwork;
