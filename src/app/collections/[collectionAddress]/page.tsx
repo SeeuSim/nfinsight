a47e125e3fbba13bdb438d5ca63040e1bb9245cc
SeeuSim <seeusimong@gmail.com>
2023-07-19 22:13:44 +0800

    Add route metadata for collection pages

--- src/app/collections/[collectionAddress]/page.tsx
+++ src/app/collections/[collectionAddress]/page.tsx
@@ -1,27 +1,43 @@
-"use client";
-
-import { useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 
 import CollectionDetails from "@/components/(collections)/CollectionDetails";
 import CollectionData from "@/components/(collections)/CollectionData";
 import MainLayout from "@/components/layouts/MainLayout";
+import { getPool } from "@/lib/database/postgres";
+import type { Metadata, ResolvingMetadata } from "next";
 
-const CollectionDetailsPage = ({
-  params,
-}: {
+type Props = {
   params: { collectionAddress: string };
-}) => {
-  const router = useRouter();
+};
+
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  // read route params
+  const address = params.collectionAddress;
+
+  const pool = getPool();
+  const name = await pool.query<{ name: string }>(
+    `SELECT name FROM collection WHERE address = $1`,
+    [address]
+  );
+
+  return {
+    title: `${name.rows[0].name} | NFInsight`,
+  };
+}
+
+const CollectionDetailsPage = async ({ params }: Props) => {
   if (
     !params.collectionAddress ||
     params.collectionAddress.match(/0x[A-Z0-9]{40,}/gi) === null
   ) {
-    router.push("/");
+    redirect("/");
   }
 
   return (
     <MainLayout>
-      <title>NFInsight - Collection</title>
       <CollectionDetails collectionAddress={params.collectionAddress} />
       <CollectionData collectionAddress={params.collectionAddress} />
     </MainLayout>
